84b41cd2818467c13c45684c5ceb9f5a10297fdd
pimentelleo <leonardociberxon@gmail.com>
2021-01-20 11:18:47 -0300

    remove use_gapps thing

--- corvus_lake.mk
+++ corvus_lake.mk
@@ -27,7 +27,6 @@ $(call inherit-product, device/motorola/lake/device.mk)
 # Inherit some common Pixel Experience stuff.
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_GAPPS := true
 
 # A/B updater
 AB_OTA_UPDATER := true
